4e1bc8a3a291420f2cbda315cd32626c4fb0748b
Dpinkney001 <duvall.pinkny@gmail.com>
2023-01-16 05:17:14 -0500

    brainstomring about me page cover letter

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Tech enthusiast
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://duvallpinkney.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My Website @ https://duvallpinkney.com
         </a>
       </header>
     </div>
